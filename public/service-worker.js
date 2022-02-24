d890946c3223620597631b1820713d53791462c4
Mark Dale <msdaledad@gmail.com>
2022-02-24 15:28:02 -0500

    service worker added...but needs to intercept fetch requests

---
+++ public/service-worker.js
@@ -0,0 +1,47 @@
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./css/style.css",
+  "./icons/icon-72X72.png",
+  "./icons/icon-96X96.png",
+  "./icons/icon-128X128.png",
+  "./icons/icon-144X144.png",
+  "./icons/icon-152X152.png",
+  "./icons/icon-192X192.png",
+  "./icons/icon-384X384.png",
+  "./icons/icon-512X512.png",
+  "./js/idb.js",
+  "./js/index.js"
+];
+
+const APP_PREFIX = 'BudgetTracker-';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeeplist = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
